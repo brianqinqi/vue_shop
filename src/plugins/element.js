f9928d6dc8f7f29a16e6529e4419e9107bae5d7c
Brian <804781626@qq.com>
2020-06-03 23:08:08 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,6 +5,8 @@ import {
   Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert,
   Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload
 } from 'element-ui'
+import Timeline from './timeline/index'
+import TimelineItem from './timeline-item/index'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -41,5 +43,7 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
