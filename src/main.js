20328a2f378e292569b836ee058d5d23407da21b
Brian <804781626@qq.com>
2020-06-03 21:34:44 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 // axios
 import axios from 'axios'
@@ -23,6 +29,19 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function (date) {
+  const dt = new Date(date)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDay() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
